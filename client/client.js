68b61b783b00f34b362c7403f4d136f5c61a4f33
Kavindu <kavindu@192.168.1.15>
2023-11-16 17:22:09 +0530

    get data through web socket

--- client/client.js
+++ client/client.js
@@ -1,1 +1,37 @@
-console.log("Hello from client");
+const webSocket = require("ws");
+
+//create a localhost client
+const client = new webSocket("ws://localhost:8000");
+
+//handling the event when connection to server is successfull
+client.on("open", () => {
+    console.log("Connected");
+
+    let jsonString = '{"data":"dummy"}';
+
+    //set timeout
+    //sendig the message to the server
+    setTimeout(() => {
+        client.send(jsonString);
+    }, 5000);
+
+    //handling the event when the websocket connection is closed
+    client.on("close", (code) => {
+        console.log(`Connection close with code: ${code}`);
+    });
+
+    //disconnectiong the websocket connection in 10s
+    setTimeout(() => {
+        client.close();
+    }, 10000);
+});                                                                                                                          
+
+//handling the event when handling the message from the server
+client.on("message", (message) => {
+    console.log(`Receved message: ${message}`);
+});
+
+//handling the event when the connection to the server is unsuccessful
+client.on("error", (error) => {
+    console.log(`Error: ${error}`);
+})
--- server/server.js
+++ server/server.js
@@ -1,1 +1,65 @@
-console.log("Hello from server");
+const webSocket = require("ws");
+const redis = require("redis");
+
+let redisClient;
+
+(async () => {
+    redisClient = redis.createClient();
+    redisClient.on('error', (error) => console.log(`Redis error: ${error}`));
+    await redisClient.connect();
+})();
+
+const server = new webSocket.Server({port: 8000});
+
+server.on("connection", (ws) => {
+    console.log('New client connected !');
+
+    //send welcome message to the client
+    ws.send("Hello this is welcome from SERVER");
+
+    try {
+        //reply
+        ws.on("message", async (message) => {
+
+            let data = JSON.parse(message);
+            // let user_id = data.user_acc_id;
+
+            let metaData = [
+                {user_acc_id: data.user_acc_id},
+                {pub_address: data.pub_address},
+                {cid: data.cid},
+                {d_url: data.d_url},
+                {gcs_url: data.gcs_url},
+                {bucket_name: data.bucket_name},
+                {file_name: data.file_name},
+            ];
+        
+            const response = { metaData };
+            await redisClient.set('metadata', JSON.stringify(response));
+
+            const chashedData = await redisClient.get('metadata');
+
+            if(chashedData) {
+                
+                ws.send(`Cached data: ${chashedData}`);
+                console.log(`Cached data: ${chashedData}`);
+                return;
+
+            } else {
+                ws.send(`No metadata found`);
+                console.log(`No metadata found`);
+            }
+
+            // Accessing user_acc_id from metaData
+            // const userAccId = metaData.find(item => 'user_acc_id' in item).user_acc_id;
+
+            // ws.send(`User ID: ${userAccId}`);
+            // console.log(`User Id: ${userAccId}`);
+        
+        });
+    } catch (error) {
+        console.log(`Error getting metadata: ${error}`);
+        return res.status(500).json({message: error});
+    }
+
+});
